e42154569502b37418cf050e13fbd5e3044b0671
Naeem Abbas <naeemabbaskiu@gmail.com>
2024-07-27 16:48:54 +0500

    This is my first commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+
+import 'dart:developer';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_notification_channel/flutter_notification_channel.dart';
+import 'package:flutter_notification_channel/notification_importance.dart';
+import 'package:we_chat/firebase_options.dart';
+import 'package:we_chat/screens/splash_screen.dart';
+
+
+// global objects for accessing device screen size
+late Size mq;
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+
+  // for setting to orientation to portrait only
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((value) {
+    _initializeFirebase();
+    runApp(const MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'We Chat',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          centerTitle: true,
+
+          elevation: 5,
+              iconTheme: IconThemeData(color: Colors.black),
+              titleTextStyle: TextStyle(
+                  color: Colors.black,fontWeight: FontWeight.normal,fontSize: 19),
+          backgroundColor: Colors.blueAccent,
+        )),
+      home: SplashScreen());
+  }
+}
+
+  _initializeFirebase() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  var result = await FlutterNotificationChannel().registerNotificationChannel(
+    description: 'For Showing Message Notification',
+    id: 'chats',
+    importance: NotificationImportance.IMPORTANCE_HIGH,
+    name: 'Chats');
+  log('\nNotification Channel Result: $result');
+}
+
---
+++ lib/models/chat_user.dart
@@ -0,0 +1,48 @@
+class ChatUser {
+  ChatUser({
+    required this.image,
+    required this.pushToken,
+    required this.about,
+    required this.name,
+    required this.createdAt,
+    required this.isOnline,
+    required this.id,
+    required this.lastActive,
+    required this.email,
+  });
+  late  String image;
+  late  String pushToken;
+  late  String about;
+  late  String name;
+  late  String createdAt;
+  late  bool isOnline;
+  late  String id;
+  late  String lastActive;
+  late  String email;
+
+  ChatUser.fromJson(Map<String, dynamic> json){
+    image = json['image'] ?? '';
+    pushToken = json['push_token'] ?? '';
+    about = json['about'] ?? '';
+    name = json['name'] ?? '';
+    createdAt = json['created_at'] ?? '';
+    isOnline = json['is_online'] ?? '';
+    id = json['id'] ?? '';
+    lastActive = json['last_active'] ?? '';
+    email = json['email'] ?? '';
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['image'] = image;
+    data['push_token'] = pushToken;
+    data['about'] = about;
+    data['name'] = name;
+    data['created_at'] = createdAt;
+    data['is_online'] = isOnline;
+    data['id'] = id;
+    data['last_active'] = lastActive;
+    data['email'] = email;
+    return data;
+  }
+}
